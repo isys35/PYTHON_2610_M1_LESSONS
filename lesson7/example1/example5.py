1f3ae02c5cc68f7526e805048a2217942e224d28
Dzmitry <isys35@mail.ru>
2022-11-17 10:54:09 +0300

    finish example

--- lesson7/example1/example5.py
+++ lesson7/example1/example5.py
@@ -2,12 +2,8 @@ user_text = input("Введите текст:")
 
 user_words = user_text.split(" ")
 count_words = [len(word) for word in user_words]
-max_words, min_words = max(count_words), min(count_words)
-index_max, index_min = count_words.index(max_words), count_words.index(min_words)
-
-
-# user_text.split()
-# len()
-# max, min
-# index
-# remove
+index_max, index_min = count_words.index(max(count_words)), count_words.index(min(count_words))
+result = ' '.join(
+    [word for index, word in enumerate(user_words) if index != index_max and index != index_min]
+)
+print(result)
